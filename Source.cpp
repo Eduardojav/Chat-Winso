2c4cf2451df629233919f84522bb5fd83f85ff68
Eduardojav <31295265+Eduardojav@users.noreply.github.com>
2017-11-01 19:57:24 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,23 @@
-#pragma comment(lib,"ws2_32.lib") //Required for WinSock
-#include <WinSock2.h> //For win sockets
-#include <string> //For std::string
-#include <iostream> //For std::cout, std::endl, std::cin.getline
+#pragma comment(lib,"ws2_32.lib") 
+#include <WinSock2.h> 
+#include <string> 
+#include <iostream> 
 
-SOCKET Connection;//This client's connection to the server
+SOCKET Connection;
 
 void ClientThread()
 {
-	char buffer[256]; //Create buffer to hold messages up to 256 characters
+	char buffer[256]; 
 	while (true)
 	{
-		recv(Connection, buffer, sizeof(buffer), NULL); //receive buffer
-		std::cout << buffer << std::endl; //print out buffer
+		recv(Connection, buffer, sizeof(buffer), NULL); 
+		std::cout << buffer << std::endl;
 	}
 }
 
 int main()
 {
-	//Winsock Startup
+	
 	WSAData wsaData;
 	WORD DllVersion = MAKEWORD(2, 1);
 	if (WSAStartup(DllVersion, &wsaData) != 0)
@@ -26,27 +26,26 @@ int main()
 		return 0;
 	}
 
-	SOCKADDR_IN addr; //Address to be binded to our Connection socket
-	int sizeofaddr = sizeof(addr); //Need sizeofaddr for the connect function
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //Address = localhost (this pc)
-	addr.sin_port = htons(1111); //Port = 1111
-	addr.sin_family = AF_INET; //IPv4 Socket
+	SOCKADDR_IN addr; 
+	int sizeofaddr = sizeof(addr); 
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET; 
 
-	Connection = socket(AF_INET, SOCK_STREAM, NULL); //Set Connection socket
-	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) //If we are unable to connect...
+	Connection = socket(AF_INET, SOCK_STREAM, NULL); 
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) 
 	{
 		MessageBoxA(NULL, "Failed to Connect", "Error", MB_OK | MB_ICONERROR);
-		return 0; //Failed to Connect
+		return 0; 
 	}
 
 	std::cout << "Connected!" << std::endl;
-	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); //Create the client thread that will receive any data that the server sends.
-
-	char buffer[256]; //256 char buffer to send message
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); 
+	char buffer[256]; 
 	while (true)
 	{
-		std::cin.getline(buffer, sizeof(buffer)); //Get line if user presses enter and fill the buffer
-		send(Connection, buffer, sizeof(buffer), NULL); //Send buffer
+		std::cin.getline(buffer, sizeof(buffer)); 
+		send(Connection, buffer, sizeof(buffer), NULL); 
 		Sleep(10);
 	}
 	return 0;
